e3051d36456f23876fc36f90765d9de26f6f9b55
Santha-1997 <santhakumarg@compunet.solutions>
2023-04-27 20:04:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import  { Muitypography } from './component/muitypography' 
+  
+function App () {
+       return ( 
+         <div className='App'>
+         <Muitypography />
+          </div> 
+     )
+  }
+
+export default App 
